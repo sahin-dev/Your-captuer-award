287ed15d8980ea9d218d9ddd0ff0f8a2ef2ddc81
sahin <sahinsiraj360@gmail.com>
2026-06-18 11:43:42 +0600

    add public endpoints

--- src/app/modules/Profile/profile.controlle.ts
+++ src/app/modules/Profile/profile.controlle.ts
@@ -1,82 +1,112 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import { handleGetUserUploads, profileService } from './profile.service'
 import sendResponse from '../../../shared/ApiResponse'
 import httpStatus from 'http-status'
 import catchAsync from '../../../shared/catchAsync'
 
 
-export const getMyUploads = async (req:Request, res:Response)=> {
+export const getMyUploads = async (req: Request, res: Response) => {
     const userId = req.user.id
 
-    const {page, limit} = req.query as { page?: string; limit?: string }
+    const { page, limit } = req.query as { page?: string; limit?: string }
 
     let pageNum = page ? Number(page) : undefined
     let limitNum = limit ? Number(limit) : undefined
 
-    const result = await handleGetUserUploads(userId, {page:pageNum, limit:limitNum})
+    const result = await handleGetUserUploads(userId, { page: pageNum, limit: limitNum })
 
     sendResponse(res, {
-        statusCode:httpStatus.OK,
-        success:true,
-        message:"user uploads fetched successfully",
-        data:result.data,
-        meta:result.meta
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "user uploads fetched successfully",
+        data: result.data,
+        meta: result.meta
     })
 }
 
+export const getUserPhotos = catchAsync(async (req: Request, res: Response) => {
+    const userId = req.params.id
+    const { page, limit } = req.query as { page?: string; limit?: string }
 
-const uploadUserPhoto = async (req:Request, res:Response) => {
+    let pageNum = page ? Number(page) : undefined
+    let limitNum = limit ? Number(limit) : undefined
+
+    const result = await handleGetUserUploads(userId, { page: pageNum, limit: limitNum })
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "user uploads fetched successfully",
+        data: result.data,
+        meta: result.meta
+    })
+})
+
+
+const uploadUserPhoto = async (req: Request, res: Response) => {
     const userId = req.user.id
     const file = req.file
     const addedPhoto = await profileService.uploadUserPhoto(userId, file as Express.Multer.File)
 
     sendResponse(res, {
-        statusCode:httpStatus.CREATED,
-        success:true,
-        message:"photo uploaded successfully",
-        data:addedPhoto
+        statusCode: httpStatus.CREATED,
+        success: true,
+        message: "photo uploaded successfully",
+        data: addedPhoto
     })
 }
 
 
-const getUserStates = async (req:Request, res:Response) => {
+const getUserStates = async (req: Request, res: Response) => {
     const userId = req.user.id
     const states = await profileService.getStates(userId)
 
     sendResponse(res, {
-        statusCode:httpStatus.OK,
-        success:true,
-        message:"user states fetched successfully",
-        data:states
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "user states fetched successfully",
+        data: states
+    })
+}
+
+const getUserPublicStates = async (req:Request, res:Response) => {
+    const userId = req.params.id
+    const states = await profileService.getStates(userId)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "user states fetched successfully",
+        data: states
     })
 }
 
 
-const getUserPhotoDetails = catchAsync(async (req:Request, res:Response) => {
-    const {photoId} = req.params
+const getUserPhotoDetails = catchAsync(async (req: Request, res: Response) => {
+    const { photoId } = req.params
     const userId = req.user.id
 
-    const result = await profileService.getUserPhotoDetails(userId,photoId)
+    const result = await profileService.getUserPhotoDetails(userId, photoId)
 
     sendResponse(res, {
-        success:true,
-        statusCode:httpStatus.OK,
-        message:"photo details fetched successfully",
-        data:result
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "photo details fetched successfully",
+        data: result
     })
 })
 
-const deleteUserPhoto = catchAsync(async (req:Request, res:Response) => {
-    const {photoId} = req.params
+const deleteUserPhoto = catchAsync(async (req: Request, res: Response) => {
+    const { photoId } = req.params
     const userId = req.user.id
 
     const result = await profileService.deleteUserPhoto(userId, photoId)
 
     sendResponse(res, {
-        success:true,
-        statusCode:httpStatus.OK,
-        message:"User photo deleted successfully",
-        data:result
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User photo deleted successfully",
+        data: result
     })
 })
 export const profileController = {
@@ -84,6 +114,8 @@ export const profileController = {
     uploadUserPhoto,
     getUserStates,
     getUserPhotoDetails,
-    deleteUserPhoto
+    deleteUserPhoto,
+    getUserPhotos,
+    getUserPublicStates
 
 }
--- src/app/modules/Profile/profile.route.ts
+++ src/app/modules/Profile/profile.route.ts
@@ -6,9 +6,10 @@ import { fileUploader } from "../../../helpers/fileUploader";
 const route = Router();
 
 route.get("/photos", auth(), profileController.getMyUploads);
-route.post("/photos/upload",auth(), fileUploader.filesystemUploadUserPhoto, profileController.uploadUserPhoto);
+route.get("/users/:id", auth(), profileController.getUserPhotos);
+route.post("/photos/upload", auth(), fileUploader.filesystemUploadUserPhoto, profileController.uploadUserPhoto);
 route.get("/stats", auth(), profileController.getUserStates);
-
+route.get("/users/:id/stats", auth(), profileController.getUserPublicStates);
 route.get("/photos/:photoId", auth(), profileController.getUserPhotoDetails)
 route.delete("/photos/:photoId", auth(), profileController.deleteUserPhoto)
 
