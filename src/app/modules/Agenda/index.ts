a11e4dc2f0f542b8c1ec0ed322dd4ca4daa4835c
sahin <sahinsiraj360@gmail.com>
2026-07-04 08:27:37 +0600

    modify recurring contest job

--- src/app/modules/Agenda/index.ts
+++ src/app/modules/Agenda/index.ts
@@ -4,7 +4,7 @@ import agenda from "./jobs"
 agenda.on('ready', async () => {
     console.log("Agenda is ready"); 
     
-    agenda.every("five minute", "contest:checkRecurring");   
+    agenda.every("1 minute", "contest:checkRecurring");   
     agenda.every("1 minute", "contest:active")
     await agenda.start();
 });
--- src/app/modules/Agenda/jobs.ts
+++ src/app/modules/Agenda/jobs.ts
@@ -155,13 +155,13 @@ async function scheduleContest(rContest: RecurringContest) {
         }
     })
 
-    const rules = rContest.rules  as ContestRule[]
+    const rules = JSON.parse(rContest.rules as string) as ContestRule[]
 
     for (const rule of rules) {
         await prisma.contestRule.create({ data: { contestId: newContest.id, name: rule.name, description: rule.description } })
     }
 
-    const prizes = rContest.prizes as ContestPrize[]
+    const prizes = JSON.parse(rContest.prizes as string) as ContestPrize[]
 
     for (const prize of prizes) {
         await prisma.contestPrize.create({ data: { contestId: newContest.id, category: prize.category, key: prize.key, boost: prize.boost, swap: (prize.swap) } })
